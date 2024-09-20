2b8d19eef4141dbcdb5a0cb2134767777546a9f5
Ariz Kazani <kazani.ariz@gmail.com>
2024-09-19 23:28:55 -0400

    tut2 files

--- Tutorials/tutorial1/p2.cc
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int main() {
-    int num1, num2;
-    std::cout<<"Please enter two integers: ";
-    std::cin>>num1>>num2;
-    std::cout<<"The product of "<<num1<<" and "<<num2<<" is "<<num1*num2<<"!"<<std::endl;
-    return 0;
-}
